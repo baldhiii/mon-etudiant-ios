9e88227971c377eae60454dbbf43d0d821e5ccbd
Moustapha Kone <moustaphakone513@gmail.com>
2026-07-19 06:14:37 +0100

    Initial Commit

---
+++ mon-etudiant-ios/mon_etudiant_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mon_etudiant_iosApp.swift
+//  mon-etudiant-ios
+//
+//  Created by Moustapha on 19/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct mon_etudiant_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
